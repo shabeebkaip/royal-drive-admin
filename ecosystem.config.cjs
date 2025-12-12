296969330a83cba8c7d3e0823b1d3110e5d6b731
Shabeeb Kaip <shabeebkaip@gmail.com>
2025-12-12 03:20:06 +0300

    Change admin app port to 3002

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 3002,
       },
       error_file: './logs/pm2-error.log',
       out_file: './logs/pm2-out.log',
@@ -18,7 +18,7 @@ module.exports = {
       max_memory_restart: '1G',
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 3002,
       },
     },
   ],
